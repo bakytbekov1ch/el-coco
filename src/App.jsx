5ad9cde97f8bea8dc7525d64e40023280906e96b
bakytbekov1ch <kalmamatovelibek32@gmail.com>
2024-09-19 14:40:30 +0600

    Apple filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import React, { useState } from "react";
+import Header from "./app/HF/header/Header";
+import Footer from "./app/HF/footer/Footer";
 import { Route, Routes } from "react-router-dom";
-import Layout from "./components/layout/Layout";
 import HomePage from "./pages/HomePage";
-import AboutPage from "./pages/AboutPage";
-import ContactPage from "./pages/ContactPage";
-import NewsPage from "./pages/NewsPage";
-import BasketPage from "./pages/BasketPage";
-import PlusPage from "./pages/PlusPage";
-import SearchPage from "./pages/SearchPage";
-import ProfilePage from "./pages/ProfilePage";
 import NotfoundPage from "./pages/NotfoundPage";
 import Auth from "./auth/Auth";
 import ProfileOne from "./components/profile-1/ProfileOne";
+import AboutPage from "./pages/AboutPage";
 import AddressPage from "./pages/AddressPage";
-import ProductLate from "./components/productlate/ProductLate";
-import Pay from "./components/pay/Pay";
-import SellOne from "./components/sellOne/SellOne";
+import BasketPage from "./pages/BasketPage";
 import Buy from "./components/buy/Buy";
+import PlusPage from "./pages/PlusPage";
+import ProfilePage from "./pages/ProfilePage";
+import Pay from "./components/pay/Pay";
+import ProductLate from "./components/productlate/ProductLate";
+import NewsPage from "./pages/NewsPage";
+import ContactPage from "./pages/ContactPage";
+import SearchPage from "./pages/SearchPage";
+import SellOne from "./components/sellOne/SellOne";
 
 const mocapi = "https://669366b5c6be000fa07b6234.mockapi.io/el-coco";
 
@@ -36,36 +37,40 @@ function App() {
   };
 
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route
-          index
-          element={
-            <HomePage
-              setSearch={setSearch}
-              filterhandle={filterhandle}
-              handleSearch={handleSearch}
-              search={search}
-            />
-          }
-        />
-        <Route path="*" element={<NotfoundPage />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/a1" element={<ProfileOne />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/address" element={<AddressPage />} />
-        <Route path="/basket" element={<BasketPage />} />
-        <Route path="/buy" element={<Buy />} />
-        <Route path="/plus" element={<PlusPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/pay" element={<Pay />} />
-        <Route path="/productlate/:id" element={<ProductLate />} />
-        <Route path="/news" element={<NewsPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="/sell" element={<SellOne />} />
-      </Route>
-    </Routes>
+    <>
+      <Header />
+      <main>
+        <Routes>
+          <Route
+            index
+            element={
+              <HomePage
+                setSearch={setSearch}
+                filterhandle={filterhandle}
+                handleSearch={handleSearch}
+                search={search}
+              />
+            }
+          />
+          <Route path="*" element={<NotfoundPage />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/a1" element={<ProfileOne />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/address" element={<AddressPage />} />
+          <Route path="/basket" element={<BasketPage />} />
+          <Route path="/buy" element={<Buy />} />
+          <Route path="/plus" element={<PlusPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/pay" element={<Pay />} />
+          <Route path="/productlate/:id" element={<ProductLate />} />
+          <Route path="/news" element={<NewsPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/sell" element={<SellOne />} />
+        </Routes>
+      </main>
+      <Footer />
+    </>
   );
 }
 
